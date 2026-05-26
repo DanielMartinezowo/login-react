237ee9b761bc7c0f58b7d3af28539925e5e891ff
DanielMartinezowo <danielmtzcastro202@gmail.com>
2026-05-26 11:59:27 -0600

    add first version of component button

---
+++ src/components/button.tsx
@@ -0,0 +1,22 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+}
+
+export function Button({ text, disabled, ...rest }: ButtonProps) {
+  return (
+    <button
+      {...rest}
+      disabled={disabled}
+      className='mt-5 
+      px-7.5 py-2.5 
+      bg-[#824fcf]
+     text-white text-[15px] font-semibold 
+      rounded-[20px] 
+      shadow-[0_2px_5px_rgba(0,0,0,0.1)] 
+      transition-colors duration-300 ease-in-out
+      hover:bg-[#cf4f9c] disabled:bg-[#ccc] disabled:cursor-not-allowed'
+    >
+      {text}
+    </button>
+  );
+}
