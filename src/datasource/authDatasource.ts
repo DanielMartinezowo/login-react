f14e103bb4895ebfe1f522c73434737bc1152703
DanielMartinezowo <daniel.martinez@siticl.com>
2026-06-25 13:46:52 -0600

    [refactor]: change the name of method in authDataSource

--- src/datasource/authDatasource.ts
+++ src/datasource/authDatasource.ts
@@ -2,10 +2,10 @@ import { users as staticUsers } from '../users/user-static';
 import type { IUser } from '../users/IUser';
 
 export interface IAuthDataSource {
-  get(email: string, password: string): Promise<IUser>;
+  login(email: string, password: string): Promise<IUser>;
 }
 export class AuthMemoryDataSource implements IAuthDataSource {
-  async get(email: string, password: string): Promise<IUser> {
+  async login(email: string, password: string): Promise<IUser> {
     const userFound = staticUsers.find((u) => u.email === email && u.password === password);
     if (userFound) {
       return userFound;
