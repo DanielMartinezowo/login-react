668d200b6123ea0b0853ac5dd926c8fd1bac9b35
DanielMartinezowo <daniel.martinez@siticl.com>
2026-06-26 12:28:13 -0600

    [refactor]: change the structure and call of users in auth

--- src/datasource/authDatasource.ts
+++ src/datasource/authDatasource.ts
@@ -1,12 +1,17 @@
-import { users as staticUsers } from '../users/user-static';
 import type { IUser } from '../users/IUser';
 
 export interface IAuthDataSource {
   login(email: string, password: string): Promise<IUser>;
 }
 export class AuthMemoryDataSource implements IAuthDataSource {
+  private users: IUser[] = [
+    { name: 'Ana García', email: 'ana.garcia@hotmail.com', password: 'token_seguro_123' },
+    { name: 'Carlos Ruiz', email: 'cruiz_dev@gmail.com', password: 'miPassword2026' },
+    { name: 'Elena Martínez', email: 'elena.mtz@gmail.com', password: 'qwerty_987' },
+    { name: 'daniel martinez', email: 'danielmtzcastro202@gmail.com', password: '123456' },
+  ];
   async login(email: string, password: string): Promise<IUser> {
-    const userFound = staticUsers.find((u) => u.email === email && u.password === password);
+    const userFound = this.users.find((u) => u.email === email && u.password === password);
     if (userFound) {
       return userFound;
     } else {
